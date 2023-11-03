31d084bb3127289f87593cc906c52b509f883e76
ViniMagaa <vinisantos2008vs@gmail.com>
2023-11-03 19:32:54 -0300

    feat: exibindo uma questão por vez

--- src/components/QuizForm/QuizForm.jsx
+++ src/components/QuizForm/QuizForm.jsx
@@ -1,26 +1,50 @@
+import { useState } from "react";
+
 import Question from "./Question/Question";
 import Button from "../Button/Button";
 
-import "./QuizForm.css"
+import "./QuizForm.css";
 
 function QuizForm({ data }) {
 	const questions = data;
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+
+	function handleLastQuestion() {
+		setCurrentQuestion((crrQuest) => crrQuest - 1);
+	}
+
+	function handleNextQuestion() {
+		setCurrentQuestion((crrQuest) => crrQuest + 1);
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
 
 	return (
-		<form onSubmit={(e) => e.preventDefault()} className="container">
+		<form onSubmit={(e) => handleSubmit(e)} className="container">
 			<h1>Quiz 1</h1>
 			<div className="container">
-				{questions.map((question, index) => (
+				{currentQuestion < questions.length && (
 					<Question
-						key={index}
-						index={index}
-						question={question.question}
-						correctAnswer={question.correct_answer}
-						incorrectAnswers={question.incorrect_answers}
+						key={currentQuestion}
+						index={currentQuestion}
+						question={questions[currentQuestion].question}
+						correctAnswer={questions[currentQuestion].correct_answer}
+						incorrectAnswers={questions[currentQuestion].incorrect_answers}
 					/>
-				))}
+				)}
 			</div>
-			<Button type="submit" value="Submit Quiz" />
+			<div className="change-questions">
+				{currentQuestion > 0 && (
+					<Button value="Previous" handleClick={handleLastQuestion} />
+				)}
+				{currentQuestion < questions.length - 1 ? (
+					<Button value="Next" handleClick={handleNextQuestion} />
+				) : (
+					<Button type="submit" value="Submit" />
+				)}
+			</div>
 		</form>
 	);
 }
